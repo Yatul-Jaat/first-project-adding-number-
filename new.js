808a6508d1be60131beea351ee093053d6457857
Yatul-Jaat <yatuljaat0212@gmail.com>
2024-10-01 19:08:52 +0530

    add my project

---
+++ new.js
@@ -0,0 +1,11 @@
+let count=document.getElementById("display-0")
+let hello=0
+let save=document.getElementById("saved-output")
+function adding(){
+    hello+=1
+    count.innerText=hello
+}
+
+function saving (){
+    save.innerText+= hello + "-"
+}
